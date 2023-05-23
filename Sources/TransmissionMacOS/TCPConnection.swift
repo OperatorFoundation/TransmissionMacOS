ef0fae8eeff399ff5ee70337baca3b7355fd9035
consuelita <adelita@operatorfoundation.org>
2023-05-23 14:02:54 -0500

    Update TCPConnection.swift

--- Sources/TransmissionMacOS/TCPConnection.swift
+++ Sources/TransmissionMacOS/TCPConnection.swift
@@ -93,8 +93,9 @@ public class TCPConnection: IPConnection
         }
         
         let start = DispatchTime.now()
-        let timeoutPeriod = start + .seconds(timeoutSeconds) // Converting timeoutSeconds to nanoseconds
-        print("⏰ TransmissionMacOS: networkRead starting timeout. Start: \(start) timeoutPeriod: \(timeoutPeriod)")
+        let timeoutTime = DispatchTimeInterval.seconds(timeoutSeconds) // Converting timeoutSeconds to nanoseconds
+        let timeoutPeriod = start + timeoutTime
+        print("⏰ TransmissionMacOS: networkRead starting timeout. \nStart: \(start)\ntimeoutTime: \(timeoutTime)\ntimeoutPeriod: \(timeoutPeriod)")
         let tcpReadResultType = tcpReadLock.wait(timeout: timeoutPeriod)
         
         switch tcpReadResultType {
