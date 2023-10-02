8b1c004d18f6fe5143e4a1e16824cc6c352a516e
bragelbytes <joebragel@gmail.com>
2023-10-02 15:23:30 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6"),
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionBase", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Transport", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionBase", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionTypes", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/Transport", branch: "release"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
